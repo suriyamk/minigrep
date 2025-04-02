eac6ba0b7a232a07b82967b0624bb61dca2f5bad
Suriya Manikandan <mjsuryavarma@gmail.com>
2025-04-02 06:23:08 +0530

    minigrep

---
+++ src/lib.rs
@@ -0,0 +1,138 @@
+//! # My Crate
+//!
+//! `my_crate` is a collection of utilities to make performing certain
+//! calculations more convenient.
+
+/// Adds one to the number given.
+// --snip--
+
+pub use self::utils::mix;
+pub use self::kinds::PrimaryColor;
+pub use self::kinds::SecondaryColor;
+
+use std::error::Error;
+use std::fs;
+use std::env;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents: String = fs::read_to_string(config.file_path)?;
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+ 
+ contents.lines()
+ .filter(|x| x.contains(query))
+ .collect()
+}
+
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str,
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
+}
+
+impl Config {
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+
+        args.next(); 
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path: String = match args.next() {
+            Some(args) => args,
+            None => return Err("Didn't get a file path string"),
+        };
+
+        let is_ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case: is_ignore_case })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sentitive() {
+        let query = "hello";
+        let contents = "\
+        Rust:\
+        Safe, fast, productive.\
+        Pick one";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insentitive() {
+        let query = "hello";
+        let contents = "\
+        Rust:\
+        Safe, fast, productive.\
+        Pick one";
+
+        assert_eq!(vec!["Safe, Fast, Productive."], search(query, contents));
+    }
+}
+
+
+pub mod kinds {
+    /// The primary colors according to the RYB color model.
+    pub enum PrimaryColor {
+        Red,
+        Yellow,
+        Blue,
+    }
+
+    /// The secondary colors according to the RYB color model.
+    pub enum SecondaryColor {
+        Orange,
+        Green,
+        Purple,
+    }
+}
+
+pub mod utils {
+    use crate::kinds::*;
+
+    /// Combines two primary colors in equal amounts to create
+    /// a secondary color.
+    pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
+        // --snip--
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    let config: Config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem processing arguments {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
